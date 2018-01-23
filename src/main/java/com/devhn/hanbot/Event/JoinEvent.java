9d243219eb2a49745b2e4a99440dcbaec3b49360
DevHN <devhn0@gmail.com>
2018-01-23 10:09:09 +0800

    Delete contents
    - Delete contents

--- src/main/java/com/devhn/hanbot/Event/JoinEvent.java
+++ src/main/java/com/devhn/hanbot/Event/JoinEvent.java
@@ -4,18 +4,13 @@ import net.dv8tion.jda.client.events.group.GroupUserJoinEvent;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 
+import java.util.HashMap;
+
 /**
  * Created by DevHN on 09/07/2017.
  */
 
 public class JoinEvent {
     public void onJoinEvent(GroupUserJoinEvent event, MessageChannel channel) {
-        User user = event.getUser();
-        boolean bot = user.isBot();
-        if (!bot) {
-            channel.sendMessage(user + " 님이 접속하셨습니다.");
-        } else {
-            channel.sendMessage("봇 " + user + " 님이 접속하셨습니다.");
-        }
     }
 }
